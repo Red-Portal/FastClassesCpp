141d12a19e8a722d9de583b9a7a01355292da8dc
Ray Kim <msca8h@naver.com>
2016-10-27 16:04:44 +0900

    First Commit

---
+++ FastClassesVSIX/EditorClassifier.cs
@@ -0,0 +1,162 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="EditorClassifier.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Utilities;
+
+namespace FastClassesVSIX
+{
+    /// <summary>
+    /// Classifier that classifies all text as an instance of the "EditorClassifier" classification type.
+    /// </summary>
+    internal class EditorClassifier : IClassifier
+    {
+        /// <summary>
+        /// Classification type.
+        /// </summary>
+        private readonly IClassificationType classificationType;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EditorClassifier"/> class.
+        /// </summary>
+        /// <param name="registry">Classification registry.</param>
+        internal EditorClassifier(IClassificationTypeRegistryService registry)
+        {
+            this.classificationType = registry.GetClassificationType("EditorClassifier");
+        }
+
+        #region IClassifier
+
+#pragma warning disable 67
+
+        /// <summary>
+        /// An event that occurs when the classification of a span of text has changed.
+        /// </summary>
+        /// <remarks>
+        /// This event gets raised if a non-text change would affect the classification in some way,
+        /// for example typing /* would cause the classification to change in C# without directly
+        /// affecting the span.
+        /// </remarks>
+        public event EventHandler<ClassificationChangedEventArgs> ClassificationChanged;
+
+#pragma warning restore 67
+
+        /// <summary>
+        /// Gets all the <see cref="ClassificationSpan"/> objects that intersect with the given range of text.
+        /// </summary>
+        /// <remarks>
+        /// This method scans the given SnapshotSpan for potential matches for this classification.
+        /// In this instance, it classifies everything and returns each span as a new ClassificationSpan.
+        /// </remarks>
+        /// <param name="span">The span currently being classified.</param>
+        /// <returns>A list of ClassificationSpans that represent spans identified to be of this classification.</returns>
+        public IList<ClassificationSpan> GetClassificationSpans(SnapshotSpan span)
+        {
+            var result = new List<ClassificationSpan>()
+            {
+                new ClassificationSpan(new SnapshotSpan(span.Snapshot, new Span(span.Start, span.Length)), this.classificationType)
+            };
+
+            return result;
+        }
+
+        #endregion
+    }
+
+    [ContentType("Code")]
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [TextViewRole(PredefinedTextViewRoles.Editable)]
+    internal sealed class TextViewCreationListener : IWpfTextViewCreationListener
+    {
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            ClassFormWriter.initializeSingletonFromParent(textView);
+        }
+    }
+
+    public sealed class ClassFormWriter
+    {
+        private IWpfTextView m_view;
+        private static ClassFormWriter classFormWriterInstance;
+        static public void initializeSingletonFromParent(IWpfTextView view)
+        {
+            classFormWriterInstance = new ClassFormWriter(view);
+        }
+        private ClassFormWriter(IWpfTextView view)
+        {
+            m_view = view;
+        }
+        static public ClassFormWriter getClassFormWriter()
+        {
+            return classFormWriterInstance;
+        }
+
+        public void fastClassOption1()
+        {
+            ITextEdit edit = m_view.TextBuffer.CreateEdit();
+            edit.Insert(0, "class ClassName\n" +
+                           "{\n" +
+                           "ClassName() = default;\n" +
+                           "~ClassName() = default;\n" +
+                           "}\n");
+            edit.Apply();
+        }
+    }
+    /*
+    class Formatter
+    {
+        private IWpfTextView _view;
+        private bool _isChangingText = false;
+        public Formatter(IWpfTextView view)
+        {
+            _view = view;
+            _view.TextBuffer.Changed += new EventHandler<TextContentChangedEventArgs>(TextBuffer_Changed);
+            _view.TextBuffer.PostChanged += new EventHandler(TextBuffer_PostChanged);
+        }
+
+        private void TextBuffer_Changed(object sender, TextContentChangedEventArgs e)
+        {
+            if (!_isChangingText)
+            {
+                _isChangingText = true;
+                FormatCode(e);
+            }
+        }
+
+        private void TextBuffer_PostChanged(object sender, EventArgs e)
+        {
+            if (_isChangingText)
+                _isChangingText = false;
+        }
+
+        private void FormatCode(TextContentChangedEventArgs e)
+        {
+            if (e.Changes != null)
+            {
+                for (int i = 0; i < e.Changes.Count; i++)
+                {
+                    HandleChange(e.Changes[0].NewText);
+                }
+            }
+
+        }
+
+        private void HandleChange(string newText)
+        {
+            ITextEdit edit = _view.TextBuffer.CreateEdit();
+            edit.Insert(0, "Hello");
+            edit.Apply();
+        }
+        */
+
+}
---
+++ FastClassesVSIX/FastClassesCpp.cs
@@ -0,0 +1,108 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="FastClassesCpp.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using Microsoft.VisualStudio.Shell;
+
+namespace FastClassesVSIX
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class FastClassesCpp
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int makeClassOption1 = 0x0100;
+        public const int makeClassOption2 = 0x0101;
+        public const int makeClassOption3 = 0x0102;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("0d389696-59eb-416b-ae57-e8ca5a691498");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly Package package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FastClassesCpp"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        private FastClassesCpp(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+            this.package = package;
+
+            OleMenuCommandService commandService = this.ServiceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var makeClassOption1_cmdID = new CommandID(CommandSet, makeClassOption1 );
+                var makeClassOption1_menuItem = new MenuCommand(this.MenuItemCallback, makeClassOption1_cmdID);
+                commandService.AddCommand(makeClassOption1_menuItem);
+
+                var makeClassOption2_cmdID = new CommandID(CommandSet, makeClassOption2);
+                var makeClassOption2_menuItem = new MenuCommand(this.MenuItemCallback, makeClassOption2_cmdID);
+                commandService.AddCommand(makeClassOption2_menuItem);
+
+                var makeClassOption3_cmdID = new CommandID(CommandSet, makeClassOption3 );
+                var makeClassOption3_menuItem = new MenuCommand(this.MenuItemCallback, makeClassOption3_cmdID);
+                commandService.AddCommand(makeClassOption3_menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static FastClassesCpp Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static void Initialize(Package package)
+        {
+            Instance = new FastClassesCpp(package);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            ClassFormWriter.getClassFormWriter().fastClassOption1();
+        }
+    }
+}
